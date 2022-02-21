42e3bbf025502a1ed534349ad80a43ba9ce2ec26
prajaktaAkolkar <prajaktaAkolkar@mangoitsolutions.in>
2022-02-21 16:07:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+let port = process.env.PORT || 3000;
+const importData = require("./data.json");
+app.get('/',(req,res)=>{
+    res.send("Hello World!!!!")
+});
+
+app.get('/players',(req,res)=>{
+    res.send(importData);
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Example app is listening on port http://localhost:${port}`);
+})
